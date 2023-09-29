be132faef3e79bc87730078b9cf5a9324486a9bb
scheinera <scheineranderson@gmail.com>
2023-09-29 17:15:16 -0300

    29092023

--- Crypto/crypto.py
+++ Crypto/crypto.py
@@ -1,5 +1,8 @@
 from binance.client import Client
-from secrets import api_secret, api_key
+# from secrets import api_secret, api_key
+api_secret = "6r6OtaF1AbvNsKfxrElFvYjzHjDTWzcZGouWrQWDHqH0aPPkE8OqCjjD2DDital9"
+api_key = "YvH7L3VgJJRw42XPyHZhlxkiK30kgvMbvOYN9hdKasKiUYSw0ZVhKfV1qi0ckIsX"
+
 
 client = Client(api_key, api_secret)
 
@@ -19,7 +22,7 @@ for ativo in lista_ativos:
 # criar uma ordem dentro da binance
 from binance.enums import *
 order = client.create_order(
-    symbol='BNBBRL',
+    symbol='XRPUSDT',
     side=SIDE_SELL,
     type=ORDER_TYPE_MARKET,
     quantity=0.01,
@@ -27,12 +30,12 @@ order = client.create_order(
 print(order)
 
 # visualizar as ordens executadas
-print(client.get_all_orders(symbol='BNBBRL'))
-print(client.get_my_trades(symbol='BNBBRL'))
+print(client.get_all_orders(symbol='XRPUSDT'))
+print(client.get_my_trades(symbol='XRPUSDT'))
 
 # te mostrar as referências de cada par de moedas
-print(client.get_symbol_info('BNBBRL'))
+print(client.get_symbol_info('XRPUSDT'))
 
 # pegar as cotações em tempo real
-transacoes = client.get_recent_trades(symbol="BNBBRL", limit=1)
+transacoes = client.get_recent_trades(symbol="XRPUSDT", limit=1)
 print(transacoes)
